e2dc3894fb614bfc3e6639cf991d72b6ae9486c0
Danulo <danilyanishevski2001@gmail.com>
2024-05-05 13:15:25 +0300

    update

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
 import path from "path";
 import * as fs from "node:fs/promises";
 
-const targetPath = path.resolve("downloads")
+const targetPath = path.resolve("downloads");
+
+const dirDict = {
+  img: [".jpg", ".svg", ".png", ".webp"],
+  doc: [".txt", ".pdf", ".xsl"],
+  arch: [".zip", ".rar"],
+  media: [".mp4", ".mp3", ".avi"],
+};
+
+const dirStorage = {};
 
 async function dirHandler(dirPath) {
-    const filesList = await fs.readdir(dirPath)
-    
-    const promisesList = filesList.map(async (fileName) => { 
-        try {
-            // console.log( " fileInfo -Start", fileName);
-            const itemPath = path.join(dirPath, fileName);
-            const isDir = (await fs.stat(itemPath)).isDirectory()
-            // console.log( " fileInfo -End", fileName);
-          
-            return {
-                isDir,
-                itemPath
-            }
+  const filesList = await fs.readdir(dirPath);
 
-        } catch (error) {
-            console.log(" fileInfo -Error:", error.message);
-        }
+  const promisesList = filesList.map(async (fileName) => {
+    try {
+      // console.log( " fileInfo -Start", fileName);
+      const itemPath = path.join(dirPath, fileName);
+      const isDir = (await fs.stat(itemPath)).isDirectory();
+      // console.log( " fileInfo -End", fileName);
+
+      return {
+        isDir,
+        itemPath,
+      };
+    } catch (error) {
+      console.log(" fileInfo -Error:", error.message);
+    }
+  });
+
+  const result = await Promise.allSettled(promisesList);
+
+  await Promise.allSettled(
+    result.map(async ({ value }) => {
+      const { isDir, itemPath } = value;
+      if (isDir) {
+        await dirHandler(itemPath);
+      } else {
+        await fileHandler(itemPath);
+      }
     })
-
-   const result = await Promise.allSettled(promisesList) ;
-    
-    result.forEach(({ value }) => {
-        const { isDir, itemPath } = value;
-        if (isDir) {
-           dirHandler(itemPath)
-        
-        } else {
-            fileHandler(itemPath);
-
-       }
-   })
+  );
 }
 
-function fileHandler(filePath) {
- console.log( "Handle file:", filePath);
+async function fileHandler(filePath) {
+  //   console.log("Handle file:", filePath);
+  //   console.log(path.parse(filePath));
+  const { ext, base } = path.parse(filePath);
+  for (let dirName in dirDict) {
+    if (dirDict[dirName].includes(ext)) {
+      //   console.log("Move to dir:", dirName, "file:", filePath);
+      console.log("filePath:", filePath);
+      console.log("pathJoin:", path.join(targetPath, dirName, base));
+      await moveFile(filePath, dirName, base);
+      return;
+    }
+  }
+  //   console.log("Move to dir:", "Other", "file:", filePath);
+  await moveFile(filePath, "other", base);
 }
-
+async function moveFile(filePath, dirName, fileName) {
+  const finallyPath = path.join(targetPath, dirName);
+  console.log("finallyPath: Start ", finallyPath);
+  if (!dirStorage[dirName]) {
+    await fs.mkdir(finallyPath);
+    dirStorage[dirName] = true;
+  }
+  console.log("finallyPath: End ", finallyPath);
+  await fs.rename(filePath, path.join(finallyPath, fileName));
+}
 dirHandler(targetPath);
-
